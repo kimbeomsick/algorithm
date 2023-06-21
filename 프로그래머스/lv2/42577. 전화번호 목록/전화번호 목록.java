9b35d9fe1483a5cdd06df278314e47e8e9e73363
김범식 <92661439+kimbeomsick@users.noreply.github.com>
2023-06-21 10:41:09 +0900

    [level 2] Title: 전화번호 목록, Time: 198.98 ms, Memory: 134 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -4,29 +4,18 @@ class Solution {
     public boolean solution(String[] phone_book) {
         
         int size = phone_book.length;
-        
-        // //길이 순으로 나열하기
-        // Arrays.sort(phone_book,(a,b)->{
-        //     return b.length()-a.length();
-        // });
+ 
         
         
-        
-        HashMap<String, Integer> map = new HashMap<>();
+        HashSet<String> set = new HashSet<>();
         for(int i = 0;i<size;i++){
-            map.put(phone_book[i], i);
+            set.add(phone_book[i]);
         }
         
         
         for(int i = 0 ;i<size;i++){
             for(int j = 0;j<phone_book[i].length();j++){
-            //     if(phone_book[i].length() > phone_book[j].length()){
-            //         if(map.containsKey(phone_book[i].substring(0,phone_book[j].length()))){
-            //             return false;
-            //         }
-            //     }
-                
-                if(map.containsKey(phone_book[i].substring(0,j))){
+                if(set.contains(phone_book[i].substring(0,j))){
                     return false;
                 }
                 
