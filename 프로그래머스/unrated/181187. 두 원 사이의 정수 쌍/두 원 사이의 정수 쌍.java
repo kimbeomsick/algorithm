5e806f181e39905a38f253be174dcac7beadf7e9
김범식 <92661439+kimbeomsick@users.noreply.github.com>
2023-08-03 02:30:09 +0900

    [unrated] Title: 두 원 사이의 정수 쌍, Time: 16.93 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
+++ 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
@@ -1,28 +1,21 @@
 import java.util.*;
 
+//1사분면 구하고 4배하면 됨 
+// x = 1에서 r2보다 작을 때까지 증가 
+// 여기서 x = 1~r2일 때 y의 최대값과 최소값 구하기 
+// + (r2-r1+1)*4
+
 class Solution {
-    public long solution(long r1, long r2) {
+    public long solution(int r1, int r2) {
         long answer = 0;
-
-        for (int i = 1; i < r2; i++) {
-            if (i < r1) {
-                answer += getMaxY(i, r2, "r2") - getMaxY(i, r1, "r1");
-            } else {
-                answer += getMaxY(i, r2, "r2");
-            }
+        
+        for(long i = 1;i<=r2;i++){
+            long b =  (long)Math.floor(Math.sqrt((i+r2)*(r2-i))); // 바깥원의 최소값
+            long a = r1-i < 0 ? (long)0.0 : (long)Math.ceil(Math.sqrt((i+r1)*(r1-i))); // 안쪽원의 최대값 
+            answer += b-a+1;
         }
-        answer *= 4;
-        answer += (r2 - r1 + 1) * 4;
-        return answer;
-    }
-
-    private int getMaxY(long x, long r, String rName) {
-        double max = Math.sqrt(r * r - x * x);
-        int maxToInt = (int) max;
-        if (rName.equals("r1") && max - maxToInt == 0.0) {
-            return maxToInt - 1;
-        } else {
-            return maxToInt;
-        }
+        
+        
+        return answer*4;
     }
 }
