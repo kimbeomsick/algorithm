f35c582112880d057b0b9abb7e0f465dc011ccb0
김범식 <92661439+kimbeomsick@users.noreply.github.com>
2023-08-07 23:29:15 +0900

    [level 3] Title: 선입 선출 스케줄링, Time: 1.68 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/lv3/12920. 선입 선출 스케줄링/선입 선출 스케줄링.java
+++ 프로그래머스/lv3/12920. 선입 선출 스케줄링/선입 선출 스케줄링.java
@@ -1,64 +1,56 @@
 import java.util.*;
 
-
-
 class Solution {
-    public int solution(int n, int[] cores) {
+     public static int solution(int n, int[] cores) {
         int answer = 0;
-        int len = cores.length; // 프로세스의 길이 
-        
-        n = n-len; // 0시간에 할 수 있는건 빼버림 
-        int right = 0;
-        for(int i = 0;i<len;i++){
-            right = Math.max(right, cores[i]); // 제일 큰 값 찾기 
-        }
-        
-        // 시간을 작업을 n번하기 위한 시간을 찾아라 
-        int left = 1 ;
-        right = right * (n/len);
-        
-        while(left+1 < right){
-            int mid = (left+right)/2;
-            int mid_work = work(mid,cores);
-            
-            if(mid_work <= n){
-                left=mid;
+
+        int min = 0; // 시간의 최소값
+        int max = cores[0]*n; // 시간의 최대값
+
+        int time = 0;
+        int m = 0; // time까지 처리한 작업량
+
+        while (min <= max) {  
+            int mid = (min+max)/2;
+
+            int count = CountWork(mid, cores);
+
+            if (count >= n) { // 해당시간까지 처리한 작업량보다 n이 크면 time과 m갱신
+                max = mid-1;   
+                time = mid;     
+                m = count; 
             }else{
-                right=mid+1;
+                min = mid+1;
             }
         }
-        
-        // left는 시간임 그 시간에 할 수 있는 작업량이 같은지 확인해야함 
-        if(work(left, cores) == n){ //해당시간에 작업을 모두 끝낼 수 있다면.. 
-            for(int i = len-1;i>=0;i--){
-                if(n/cores[i] == 0){
-                    return i+1;
+
+        m-=n; // 처리한 작업량과 n의 차이 갱신
+        for(int i = cores.length-1; i>=0; i--){
+            if (time % cores[i] == 0) { // 시간이 time일때, 작업을 처리하는 core
+                if (m == 0) {
+                    answer = i+1;
+                    break;
                 }
-            }
-        }else{ // 작업을 전부 끝내지 못하면.. 
-            int cnt =n - work(left,cores);
-            for(int i = 0;i<len;i++){
-                if(n/cores[i] == 0){
-                    cnt--;    
-                }
-                
-                if(cnt == 0){
-                    return i;
-                }
+                m--;
             }
         }
-            
-        
-        
-        return left;
+
+        return answer;
     }
-    
-    // time에 할 수 있는 작업량 찾기
-    public static int work(int time, int[] cores){
-        int result = 0;
-        for(int i = 0;i<cores.length;i++){
-            result += time/cores[i];
+
+    static int CountWork(int time, int[] cores){
+
+        if (time == 0) { // 시간이 0일 때, 처리할 수 있는 작업량은 코어의 개수 
+            return cores.length;
         }
-        return result;
+
+        int count = cores.length; // 시간이 0일 때, 처리한 작업량 
+
+        for(int i = 0; i<cores.length; i++){ // time까지 코어가 처리할 수 있는 작업량 합산
+            count += (time/cores[i]);
+        }
+
+        return count;
     }
+
 }
