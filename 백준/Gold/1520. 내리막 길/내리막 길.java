882173eb9a863376cf65bbe2a1818f99540b9b90
김범식 <92661439+kimbeomsick@users.noreply.github.com>
2023-11-12 01:07:04 +0900

    [Gold III] Title: 내리막 길, Time: 336 ms, Memory: 36916 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -1,58 +1,66 @@
-
+import java.io.*;
 import java.util.*;
 
-public class Main {
-    static int N, M, result;
-    static int[][] visited, dp, map;
-    static int[] dr = {1, 0, -1, 0};
-    static int[] dc = {0, 1, 0, -1};
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        N = sc.nextInt();
-        M = sc.nextInt();
-        result = 0;
-
-        visited = new int[N][M]; //0은 아직 방문 안한거임
-        dp = new int[N][M]; //나에게로 오는 수 의 dp, 계산은 이 배열에서 전부 다함 
-        map = new int[N][M]; //지도 초기화
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                map[i][j] = sc.nextInt();
-                dp[i][j] = -1;
-            }
-        }
 
 
-        result = dfs(N-1,M-1);
-        System.out.println(result);
+class Main{
+	static int[][] dp ; //경로 경우의 수를 담기위한 배열
+	static int[] dr = {0,1,-1,0};
+	static int[] dc = {1,0,0,-1};
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int R = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		
+		dp = new int[R][C]; 		
+		int[][] map = new int[R][C];
+		for(int i = 0;i<R;i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0;j<C;j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		
+		// -1로 모든 지역을 초기화 해주므로써 visited를 대신함 
+		for(int[] d : dp) {
+			Arrays.fill(d,-1);
+		}
+	
+		//결과값 출력
+		System.out.println(func(map,0,0,R,C));
+		
+	}
+	
+	static public int func(int[][] map, int r, int c,int R, int C) {
+		
+		//도달하면 1 리턴
+		if(r == R-1 && c == C-1) {
+			return 1; 
+		}
+		
+		// -1이 아니라는건 이길로 이미 목표까지 가봤다는 뜻 => 0이면 못간다는뜻
+		if(dp[r][c] != -1) {
+			return dp[r][c];
+		}
+		
+		// 길은 맞는데 이길로 목표지점에 도달할수 있을지는 모르니까 0
+		dp[r][c] = 0; 
+		for(int i = 0;i<4;i++) {
+			int nr = r + dr[i];
+			int nc = c + dc[i];
+			
+			if(nr < 0 || nc < 0 || nr >=R || nc >=C) continue;
+			if(map[r][c] <= map[nr][nc] ) continue;
 
-
-    }
-
-    static int dfs(int r, int c) {
-        if (r == 0 && c == 0) { //목표지점에 도달 했을 때
-            return 1;
-        }
-
-
-        //방문한곳 0으로 초기화
-        dp[r][c] = 0;
-
-        for (int i = 0; i < 4; i++) {
-            int nr = r + dr[i];
-            int nc = c + dc[i];
-
-            //거르는 조건
-            if (nr < 0 || nr >= N || nc < 0 || nc >= M) continue; //지도를 벗어난다면 탈출
-            if (map[r][c] >= map[nr][nc]) continue; //자기보다 작거나 같으면 통과
-
-            //-1이면 아직 dfs가 이루어지지 않았다는것 ,dp에 값이 있으면 그거 가져옴
-            dp[r][c] += (dp[nr][nc] == -1) ? dfs(nr, nc) : dp[nr][nc];
-        }
-
-        return dp[r][c];
-
-    }
+			//r, c 보다 낮은지점 nr,nc 로 갔을때 목표지점에 도달할 수 있는 경우의 수를 dp[r][c]에 저장
+			dp[r][c] += func(map, nr,nc,R,C);
+		}
+		
+		//결과 값 반환
+		return dp[r][c];
+	}
 }
+
